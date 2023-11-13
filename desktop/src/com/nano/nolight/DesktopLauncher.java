db0ab149c15b5ca22aed0a0fa06613668ea59016
mawawa <nanashongwe@gmail.com>
2023-11-13 12:07:30 +0200

    Game dev also ...here we go again...

---
+++ desktop/src/com/nano/nolight/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.nano.nolight;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.nano.nolight.NoLight;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("No Light");
+		new Lwjgl3Application(new NoLight(), config);
+	}
+}
