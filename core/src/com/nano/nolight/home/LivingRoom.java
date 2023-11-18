9fdf69046e4609d1dfa9277294efcb292939679e
coffee <nanashongwe@gmail.com>
2023-11-18 09:25:01 +0200

    living room background on play ...yeah

--- core/src/com/nano/nolight/home/LivingRoom.java
+++ core/src/com/nano/nolight/home/LivingRoom.java
@@ -1,12 +1,34 @@
 package com.nano.nolight.home;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.nano.nolight.interfaces.GameObject;
 
 public class LivingRoom implements GameObject {
+    private final OrthographicCamera camera;
+    private final int height;
+    private final int width;
+    OrthogonalTiledMapRenderer mapRenderer;
+    TiledMap map;
+
+    public LivingRoom(){
+        this.width = 1920;
+        this.height = 1080;
+        this.camera = new OrthographicCamera();
+        camera.setToOrtho(false, width, height);
+        map = new TmxMapLoader().load( "assets/home/Tiled/livingroom.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map);
+
+    }
+
     @Override
     public void render() {
-
+        camera.update();
+        mapRenderer.setView(camera);
+        mapRenderer.render();
     }
 
     @Override
